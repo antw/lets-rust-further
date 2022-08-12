e13ce84b245dcc81ca27940c0fc4365183527e54
Anthony Williams <hi@antw.dev>
2022-08-12 19:01:22 +0100

    Use json macro in /v1/healthcheck

--- src/handlers/healthcheck.rs
+++ src/handlers/healthcheck.rs
@@ -1,21 +1,16 @@
 use std::sync::Arc;
 
 use axum::{extract::Extension, response::IntoResponse, Json};
-use serde::Serialize;
+use serde_json::json;
 
 pub(crate) async fn healthcheck(
     Extension(app): Extension<Arc<crate::Application>>,
 ) -> impl IntoResponse {
-    Json(HealthcheckResponse {
-        status: "available".to_string(),
-        environment: app.config.env.clone(),
-        version: crate::VERSION.to_string(),
-    })
-}
-
-#[derive(Serialize)]
-struct HealthcheckResponse {
-    status: String,
-    environment: String,
-    version: String,
+    Json(json!({
+        "status": "available",
+        "system_info": {
+            "environment": app.config.env,
+            "version": crate::VERSION,
+        },
+    }))
 }
